7fb19fdf993ce172c58b17fc1d4b5065a48dcd26
lexxA87 <alex.o.nikonov@gmail.com>
2022-12-12 23:54:09 +0400

    Changed ComicAnalyzer linq expr. and changed switch in main method

--- ComicAnalyzer.cs
+++ ComicAnalyzer.cs
@@ -14,20 +14,28 @@
             IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
         {
             IEnumerable<IGrouping<PriceRange, Comic>> grouped =
-                from comic in comics
-                orderby prices[comic.Issue]
-                group comic by CalculatePriceRange(comic, prices) into priceGroup
-                select priceGroup;
+                comics.OrderBy(comic => prices[comic.Issue])
+                .GroupBy(comic => CalculatePriceRange(comic, prices));
+
+            //from comic in comics
+            //orderby prices[comic.Issue]
+            //group comic by CalculatePriceRange(comic, prices) into priceGroup
+            //select priceGroup;
             return grouped;
         }
 
         public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
         {
-            var joined =
-                from comic in comics
-                orderby comic.Issue
-                join review in reviews on comic.Issue equals review.Issue
-                select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+            var joined = comics
+                .OrderBy(comic => comic.Issue)
+                .Join(reviews,
+                comic => comic.Issue,
+                review => review.Issue,
+                (comic, review) => $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}");
+            //from comic in comics
+            //orderby comic.Issue
+            //join review in reviews on comic.Issue equals review.Issue
+            //select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
             return joined;
         }
     }
